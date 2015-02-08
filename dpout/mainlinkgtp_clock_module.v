72a446130de0797207f0fb402ec42cd90798955a
Julius Schmidt <aiju@phicode.de>
2015-02-08 16:48:33 +0100

    added ppurom.dat, fixed autogenerated code

--- dpout/mainlinkgtp_clock_module.v
+++ dpout/mainlinkgtp_clock_module.v
@@ -63,6 +63,7 @@
 //----------------------------------------------------------------------------
 // primary         100.000            0.010
 
+`default_nettype wire
 `timescale 1ps/1ps
 (* X_CORE_INFO = "mainlinkgtp,gtwizard_v3_3,{protocol_file=Start_from_scratch}" *)
 (* CORE_GENERATION_INFO = "clk_wiz_v2_1,clk_wiz_v2_1,{component_name=clk_wiz_v2_1,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,feedback_source=FDBK_AUTO,primtype_sel=MMCM_ADV,num_out_clk=1,clkin1_period=10.0,clkin2_period=10.0,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,use_status=false,use_freeze=false,use_clk_valid=false,feedback_type=SINGLE,clock_mgr_type=MANUAL,manual_override=false}" *)
